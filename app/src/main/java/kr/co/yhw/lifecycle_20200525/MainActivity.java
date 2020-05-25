b7f821aeae5b9f3b03476d7e0d7fc24cac25eb48
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-25 14:55:35 +0900

    버튼 하나 추가하고 두번째 액티비티로 이동

--- app/src/main/java/kr/co/yhw/lifecycle_20200525/MainActivity.java
+++ app/src/main/java/kr/co/yhw/lifecycle_20200525/MainActivity.java
@@ -1,16 +1,23 @@
 package kr.co.yhw.lifecycle_20200525;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import kr.co.yhw.lifecycle_20200525.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         setupEvents();
         setValues();
         Log.d("생명주기확인","onCreate실행");
@@ -18,7 +25,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        binding.moveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext,SecondActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
     @Override
